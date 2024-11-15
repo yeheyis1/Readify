d8e8ba21f95b054d1727a2e924f3c658a739155e
Yeheyis Getachew <68479765+yeheyis1@users.noreply.github.com>
2024-11-14 20:14:43 -0500

    prod

--- server/server.js
+++ server/server.js
@@ -40,6 +40,14 @@ const startApolloServer = async () => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
   });
 
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
   // Start Express server once the database connection is open
   db.once('open', () => {
     app.listen(PORT, () => {
